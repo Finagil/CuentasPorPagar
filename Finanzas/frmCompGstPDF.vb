ea6a8412a0972f125148cb6cd4517201338a28a0
ecacerest <ecacerest@finagil.com.mx>
2020-02-19 13:39:06 -0600

    ADD RUTA tmp

--- Finanzas/frmCompGstPDF.vb
+++ Finanzas/frmCompGstPDF.vb
@@ -5,10 +5,10 @@
 
     Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
         If e.ColumnIndex = 0 Then
-            System.Diagnostics.Process.Start("\\server-raid2\TmpFinagil\CXP\" & varGlobal_IdEmpresa & "-" & DataGridView1.Rows(e.RowIndex).Cells(0).Value & ".pdf")
+            System.Diagnostics.Process.Start(My.Settings.RutaTMP & "CXP\" & varGlobal_IdEmpresa & "-" & DataGridView1.Rows(e.RowIndex).Cells(0).Value & ".pdf")
         End If
         If e.ColumnIndex = 6 Then
-            System.Diagnostics.Process.Start("\\server-raid2\TmpFinagil\GTS\" & varGlobal_IdEmpresa & "-" & DataGridView1.Rows(e.RowIndex).Cells(6).Value & ".pdf")
+            System.Diagnostics.Process.Start(My.Settings.RutaTMP & "GTS\" & varGlobal_IdEmpresa & "-" & DataGridView1.Rows(e.RowIndex).Cells(6).Value & ".pdf")
         End If
     End Sub
 End Class
